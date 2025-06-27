fbabf3c61f173bdfd1255b87e1ab1b98a1de25f6
Andry Bagus Dharmawan <33278703+andrydharmawan@users.noreply.github.com>
2025-06-27 20:56:05 +0800

    update

--- src/hooks/use-api-store.ts
+++ src/hooks/use-api-store.ts
@@ -1,7 +1,13 @@
 import { getApiStore } from "../lib/api-store";
 import { ApiMethod, UseCallReturnType } from "../types";
 
-export const useApiStore = <DReq, DRes>(api: ApiMethod<DReq, DRes>, key: string): UseCallReturnType<DReq, DRes> => {
+type InferApiRequest<T> = T extends ApiMethod<infer Req, any> ? Req : never;
+type InferApiResponse<T> = T extends ApiMethod<any, infer Res> ? Res : never;
+
+export const useApiStore = <T extends ApiMethod<any, any>>(
+    api: T,
+    key: string
+): UseCallReturnType<InferApiRequest<T>, InferApiResponse<T>> => {
     const store = getApiStore(key);
     return store.useStore();
-}
+};
